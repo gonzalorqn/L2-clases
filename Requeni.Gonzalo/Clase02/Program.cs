91187dffca6ca21eacd67d806fc1ee427cb40ac0
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2019-03-20 11:45:10 -0300

    Add files via upload

---
+++ Requeni.Gonzalo/Clase02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Sello.mensaje = "hola";
+            Console.WriteLine(Sello.Imprimir());
+            Console.ReadLine();
+
+            Sello.color = ConsoleColor.DarkGreen;
+            Sello.ImprimirEnColor();
+            Console.ReadLine();
+            
+            Sello.ImprimirEnColor();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Requeni.Gonzalo/Clase02/Sello.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02
+{
+    public class Sello
+    {
+        public static string mensaje;
+        public static ConsoleColor color;
+
+        public static string Imprimir()
+        {
+            return Sello.ArmarFormatoMensaje();
+        }
+
+        public static void Borar()
+        {
+            Sello.mensaje = "";
+        }
+
+        public static void ImprimirEnColor()
+        {
+            Console.ForegroundColor = Sello.color;
+            Console.WriteLine(Sello.Imprimir());
+            Sello.color = ConsoleColor.Gray;
+        }
+
+        private static string ArmarFormatoMensaje()
+        {
+            string retorno = "";
+            for(int i=0;i<Sello.mensaje.Length +2 ;i++)
+            {
+                retorno += "*";
+            }
+            return retorno + "\n*" + Sello.mensaje + "*\n" + retorno;
+        }
+    }
+}
